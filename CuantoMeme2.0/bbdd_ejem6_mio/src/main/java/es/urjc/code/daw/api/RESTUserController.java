1fe7303bb8826526b20efa45a219eb102b8c5b14
Javi <jjmeiras@gmail.com>
2017-03-26 17:45:48 +0200

    Se va avanzando
    Ya va funcionando la cosa, aunque los POST me dan NullPointException

--- CuantoMeme2.0/bbdd_ejem6_mio/src/main/java/es/urjc/code/daw/api/RESTUserController.java
+++ CuantoMeme2.0/bbdd_ejem6_mio/src/main/java/es/urjc/code/daw/api/RESTUserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonView;
@@ -21,6 +22,7 @@ import es.urjc.code.daw.user.UserComponent;
 import es.urjc.code.daw.user.UserRepository;
 
 @RequestMapping("/api/")
+@RestController
 public class RESTUserController {
 	
 	private final StorageService storageService;
