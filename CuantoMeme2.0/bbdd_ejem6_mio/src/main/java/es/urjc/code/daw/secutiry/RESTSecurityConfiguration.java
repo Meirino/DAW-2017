83b56d57119581ffc767bee2ad8642146cb68a40
Javi <jjmeiras@gmail.com>
2017-04-01 13:39:49 +0200

    Cambio en la seguridad

--- CuantoMeme2.0/bbdd_ejem6_mio/src/main/java/es/urjc/code/daw/secutiry/RESTSecurityConfiguration.java
+++ CuantoMeme2.0/bbdd_ejem6_mio/src/main/java/es/urjc/code/daw/secutiry/RESTSecurityConfiguration.java
@@ -21,24 +21,7 @@ public class RESTSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.antMatcher("/api/**");
     	
     	// Public pages
-		
-				/* Usuario */
-        http.authorizeRequests().antMatchers(HttpMethod.POST ,"/api/signup").permitAll();
-        
-        		/* Tags */
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/tags").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/tags/{nombre}").permitAll(); //Da problemas
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/tags/{id}").permitAll();
-        
-        		/* Comentario */
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/comentarios").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/comentarios/{id}").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/comentariosByUser/{id}").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/comentariosByVineta/{id}").permitAll();
-        
-        		/* Viñetas */
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/vinetaspage/").permitAll();
-        http.authorizeRequests().antMatchers(HttpMethod.GET ,"/api/vinetas/{id}").permitAll();
+		http.authorizeRequests().anyRequest().permitAll();
         
         // Métodos que requieren autenticación
         
