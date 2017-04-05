8dc651525c56b305f75db4848d93ad4cc30e837d
Javi <jjmeiras@gmail.com>
2017-04-06 01:44:45 +0200

    Añadido fuerza de una contraseña
    Añadido al componente de registro un algoritmo para comprobar la fuerza
    de la contraseña introducida por el usuario

---
+++ CuantoMeme/src/app/classes/mockData.class.ts
@@ -0,0 +1,1 @@
+import { Vineta } from './Vineta.class';
--- CuantoMeme/src/app/signup.component.ts
+++ CuantoMeme/src/app/signup.component.ts
@@ -5,10 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './templates/signup.template.html',
   styleUrls: ['./templates/css/app.component.css']
 })
+
 export class SignUpComponent {
   //Comentario
   nombre: string = '';
   email: string = '';
   pass: string = '';
-  pass2: string = ''; 
+  fuerzaPass: string;
+
+  getPassStrength(): void {
+    //Inicializo su fuerza a 0
+    let fuerza: number = 0;
+
+    if(this.pass.length > 0) {
+      fuerza = (5 * this.pass.length);
+
+      //Si contiene números entonces le doy puntos
+      if(this.pass.replace(/[^0-9]/g,"").length > 0) {
+        fuerza = fuerza + 20;
+      }
+
+      //Si la fuerza es mayor de cincuenta es media
+      if(fuerza > 0) {
+        this.fuerzaPass = "baja";
+      } if(fuerza >= 30) {
+        this.fuerzaPass = "media";
+      } if(fuerza >= 60) {
+        this.fuerzaPass = "fuerte";
+      } if(fuerza >= 90) {
+        this.fuerzaPass = "muy fuerte";
+      }
+
+    } else {
+      this.fuerzaPass = "nula";
+    }
+    
+  }
+
 }
