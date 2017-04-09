5ad9bfe717a33364f0e5039b9ef53c3da912d432
Javi <jjmeiras@gmail.com>
2017-04-09 20:26:49 +0200

    Error solucionado

--- CuantoMeme/src/app/classes/Vineta.class.ts
+++ CuantoMeme/src/app/classes/Vineta.class.ts
@@ -8,24 +8,14 @@ export class Vineta {
     public imgURL: string;
     public likes: number;
     public dislikes: number;
-    public userAvatar: string;
-    public userID: number;
-    public username: string;
-    public tagname: string;
-    public comentarios: Comentario[];
 
-    constructor(id: number, titulo: string, descripcion: string, imgURL: string, likes: number, dislikes: number, useravatar: string, userID: number, username: string, tagname: string, comentarios: Comentario[]) {
+    constructor(id: number, titulo: string, descripcion: string, imgURL: string, likes: number, dislikes: number) {
         this.id = id;
         this.titulo = titulo;
         this.descripcion = descripcion;
         this.imgURL = imgURL;
         this.likes = likes;
         this.dislikes = dislikes;
-        this.userAvatar = useravatar;
-        this.userID = userID;
-        this.username = username;
-        this.tagname = tagname;
-        this.comentarios = comentarios;
     }
     
 }
