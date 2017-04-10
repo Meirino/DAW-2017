8769f0b142c95e965b8e779c194ce38253dc7de4
Javi <jjmeiras@gmail.com>
2017-04-11 01:33:11 +0200

    Terminada la búsqueda de viñetas

--- CuantoMeme/src/app/busquedaComponent.component.ts
+++ CuantoMeme/src/app/busquedaComponent.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vineta } from './classes/Vineta.class';
+import { VinetasService } from './services/vinetas.service';
 
 @Component({
   selector: 'busq-component',
-  template: '<h3>{{title}}</h3>',
-  styleUrls: ['./templates/css/app.component.css']
+  templateUrl: './templates/busqueda.template.html',
+  styleUrls: ['./templates/css/busqueda.css']
 })
+
 export class BusquedaComponent {
-  title = '¡Componente 2!';
+  title = 'Búsqueda avanzada';
+  resultado: Vineta[] = [];
+  texto: string;
+  modo: string = "titulo";
+  cuatrocientoscuatro: boolean = false;
+
+  constructor(private servicioViñeta: VinetasService) {
+    //etc
+  }
+
+  buscar() {
+    this.servicioViñeta.busqVinetas(this.texto, this.modo).subscribe(
+      vinetas => this.resultado = vinetas,
+      error => function() {
+        console.error(error);
+        this.cuatrocientoscuatro = true;
+        this.resultado = [];
+      }
+    );
+  }
+
 }
