f1e782b78c62ac469a08bd0059faf0e2b14ecd4f
Daniel Barrientos <rd.barrientosa@gmail.com>
2017-04-10 16:11:59 +0200

    ahora se pueden obtener las vinetas de manera individual con la info del comentario

--- CuantoMeme/src/app/services/vinetas.service.ts
+++ CuantoMeme/src/app/services/vinetas.service.ts
@@ -1,6 +1,6 @@
 import { Vineta } from '../classes/Vineta.class';
 import { Usuario } from '../classes/Usuario.class';
-
+import { Comentario } from '../classes/Comentario.class';
 import { Injectable } from '@angular/core';
 import { Http, Response, JsonpModule } from '@angular/http';
 
@@ -18,15 +18,42 @@ export class VinetasService {
             response => this.generateVinetas(response.json())//this.extractVinetas(response)
         )
     }
-  generateVinetas(vinetas: any[]){
+    
+    getVineta(id: number){
+        return this.http.get(BASE_URL+id).map(
+            response => this.generateVinetaWithComents(response.json())//this.extractVinetas(response)
+        )
+    }
+    
+    generateVinetas(vinetas: any[]){
       var lv: Vineta[] = [];
-      var autor : Usuario;
       for (let vineta of vinetas) {
-          autor = new Usuario(vineta.autor.id, vineta.autor.username, vineta.autor.AvatarURL)
-          lv.push(new Vineta(vineta.id, vineta.titulo, vineta.descripcion, vineta.URL, vineta.likes, vineta.dislikes, autor));
-         } 
-         console.log(lv);
+          lv.push(this.generateVineta(vineta));
+         }
+         console.log(lv); 
       return lv;
-  }
+    }
 
+    generateVineta(vineta: any){
+        var autor : Usuario = this.generateAutor(vineta.autor)
+        return new Vineta(vineta.id, vineta.titulo, vineta.descripcion, vineta.URL, vineta.likes, vineta.dislikes, autor);
+    }
+    generateVinetaWithComents(vineta: any){
+        var autor : Usuario = this.generateAutor(vineta.autor)
+        var comentarios : Comentario[] = [];
+        for(let comentario of vineta.comentarios){
+            comentarios.push(this.generateComentario(comentario));
+        }
+        var vineta2 : Vineta = new Vineta(vineta.id, vineta.titulo, vineta.descripcion, vineta.URL, vineta.likes, vineta.dislikes, autor);
+        vineta2.setComentarios(comentarios);
+        return vineta2
+        //return new Vineta(vineta.id, vineta.titulo, vineta.descripcion, vineta.URL, vineta.likes, vineta.dislikes, autor);
+    }
+    generateAutor(autor: any){
+        return new Usuario(autor.id, autor.username, autor.AvatarURL);
+    }
+    generateComentario(comentario: any){
+        var autor : Usuario = this.generateAutor(comentario.autor_comentario);
+        return new Comentario(comentario.id, comentario.fecha, comentario.comentario, autor);
+    }
 }
