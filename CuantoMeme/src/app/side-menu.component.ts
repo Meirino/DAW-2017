b17d926c14bebc9f9de69038c4b7b9e3671c7327
Javi <jjmeiras@gmail.com>
2017-04-11 02:54:42 +0200

    Mejorado el CSS del menú principal

--- CuantoMeme/src/app/side-menu.component.ts
+++ CuantoMeme/src/app/side-menu.component.ts
@@ -4,7 +4,7 @@ import { Router, ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'side-menu',
   templateUrl: './templates/sidemenu.template.html',
-  styleUrls: ['./templates/css/sidemenu.css']
+  styleUrls: ['./templates/css/sidemenu.css', './templates/font-awesome/css/font-awesome.css']
 })
 
 export class sideMenuComponent {
