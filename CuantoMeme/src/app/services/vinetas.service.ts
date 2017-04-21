16ea166b1ad18d24797d43f7b682c066fa3fecc8
Daniel Barrientos <rd.barrientosa@gmail.com>
2017-04-22 01:37:36 +0200

    modificado los services y la clase application del backend

--- CuantoMeme/src/app/services/vinetas.service.ts
+++ CuantoMeme/src/app/services/vinetas.service.ts
@@ -4,7 +4,7 @@ import { Comentario } from '../classes/Comentario.class';
 import { Tag } from '../classes/Tag.class';
 
 import { Injectable } from '@angular/core';
-import { Http, Response, JsonpModule, RequestOptions } from '@angular/http';
+import { Http, Response, JsonpModule, RequestOptions, Headers } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -13,24 +13,25 @@ import 'rxjs/Rx';
 
 
 const BASE_URL = 'http://localhost:8080/api/vinetas/'
+//Es necesario que no termine en / ya que podemos meter filtros para el paginado
 
 @Injectable()
 export class VinetasService {
     constructor(private http: Http){}
     
     getVinetas(page:number){
-        return this.http.get(BASE_URL+"?page="+page)
+        return this.http.get(BASE_URL+"?page="+page, { withCredentials: true })
         .map(response => this.generateVinetas(response.json()))
         .catch(error => this.handleError(error))
     }
 
     likeVineta(id: number) {
 
-        let headers = new Headers({
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-        });
-        let options = new RequestOptions(headers);
+    const headers = new Headers({
+      'Content-Type': 'application/json',
+      'X-Requested-With': 'XMLHttpRequest'
+    });
+    const options = new RequestOptions({ withCredentials: true, headers });
         console.log("llego al put")
             return this.http.put(BASE_URL+"like/"+id, options)
                 .map(response => response)
@@ -49,7 +50,7 @@ export class VinetasService {
     }
     
     getVineta(id: number){
-        return this.http.get(BASE_URL+id).map(
+        return this.http.get(BASE_URL+id, { withCredentials: true }).map(
             response => this.generateVinetaWithComents(response.json())//this.extractVinetas(response)
         )
         .catch(error => this.handleError(error))
